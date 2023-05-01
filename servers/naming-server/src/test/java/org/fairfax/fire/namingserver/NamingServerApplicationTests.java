e6c0e38db7d0e80ea59bb6ce2a101c6cd32f5e27
Louis Drotos <louis.drotos@gmail.com>
2023-04-30 20:54:27 -0400

    Added a Eureka naming server

---
+++ servers/naming-server/src/test/java/org/fairfax/fire/namingserver/NamingServerApplicationTests.java
@@ -0,0 +1,13 @@
+package org.fairfax.fire.namingserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NamingServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
